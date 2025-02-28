0ce2a517d610347dd9e6b357b6b2968bb653f46f
Landon F <landon.farrar@yahoo.com>
2025-02-28 13:11:18 -0700

    Finished Assignment

--- assembly/homework/homework.asm
+++ assembly/homework/homework.asm
@@ -8,3 +8,39 @@
 # this!)
 #
 ##################################################################
+.text
+main:
+    li $v0, 4
+    la $a0, prompt1
+    syscall
+
+
+    li $v0, 5
+    syscall
+    move $t0, $v0
+
+    li $v0, 4
+    la $a0, prompt2
+    syscall
+
+    li $v0, 5
+    syscall
+    move $t1, $v0
+
+    mul $t2, $t0, $t1  # $t2 = $t0 * $t1
+
+    li $v0, 4
+    la $a0, result_msg
+    syscall
+
+    li $v0, 1
+    move $a0, $t2
+    syscall
+
+    li $v0, 10
+    syscall
+
+.data
+prompt1:    .asciiz "Enter first number: "
+prompt2:    .asciiz "Enter second number: "
+result_msg: .asciiz "Multiplication result: "
